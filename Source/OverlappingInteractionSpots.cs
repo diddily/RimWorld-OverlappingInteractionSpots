1fc89403b3832f15cd320a0b03a2453bc71ca710
diddily <diddily@gmail.com>
2020-03-09 22:11:39 -0400

    First release

---
+++ Source/OverlappingInteractionSpots.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using RimWorld;
+using Verse;
+
+namespace OverlappingInteractionSpots
+{
+	[StaticConstructorOnStartup]
+	public class OverlappingInteractionSpots : Mod
+	{
+		public OverlappingInteractionSpots(ModContentPack content)
+			: base(content)
+		{
+			new Harmony("diddily.OverlappingInteractionSpots").PatchAll(Assembly.GetExecutingAssembly());
+		}
+	}
+	
+	[HarmonyPatch(typeof(PlaceWorker_PreventInteractionSpotOverlap), "AllowsPlacing")]
+	class PlaceWorker_PreventInteractionSpotOverlap_AllowsPlacing
+	{
+		public static bool Prefix(ref AcceptanceReport __result)
+		{
+			__result = true;
+			return false;
+		}
+	}
+}  
